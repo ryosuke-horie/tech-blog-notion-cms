58c746d5af482c0b26cd2949d0cd41584a1a9592
ryosuke-horie <ryosuke03140@gmial.com>
2023-09-20 00:37:18 +0900

    :shirt: JSDocをlib以下に記述

--- lib/blog-helper.ts
+++ lib/blog-helper.ts
@@ -1,3 +1,11 @@
+/**
+ * ページリンクを取得する
+ * tagがある場合はタグページへのリンクを返す
+ * ない場合は通常のページへのリンクを返す
+ * @param tag
+ * @param page
+ * @returns
+ */
 export const getPageLink = (tag: string, page: number) => {
   return tag ? `/posts/tag/${tag}/page/${page}` : `/posts/page/${page}`
 }
